94ff9bf65b2aa509c08741e18f3a0746e071880e
trannhatnam2707 <tran.nam27072003@gmail.com>
2025-07-29 23:08:23 +0700

    day1 react basic

---
+++ day1 react/hello-react/src/App.js
@@ -0,0 +1,12 @@
+import HelloWorld from "./Components/HelloWorld";
+
+function App() {
+  return (
+    <div>
+      <HelloWorld name = "Nam" />
+      <HelloWorld name = 'Hải' />
+    </div>
+  );
+}
+
+export default App;
---
+++ day1 react/hello-react/src/Components/HelloWorld.js
@@ -0,0 +1,10 @@
+function HelloWorld(props) {
+    return (
+        <>
+            <h1>Hello, {props.Name}</h1>
+            <p>You are learning React</p>
+        </>
+    );
+}
+
+export default HelloWorld;
